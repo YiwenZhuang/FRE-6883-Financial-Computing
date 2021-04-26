99a4562eb3475eb1f6e69cded84dbe0c044f50c7
YiwenZhuang <81824043+YiwenZhuang@users.noreply.github.com>
2021-04-26 12:43:19 -0500

    Add files via upload

---
+++ HW8_yz6735/HW8_Q1/HW8_Q1/Solver03.cpp
@@ -0,0 +1,8 @@
+//
+//  Solver03.cpp
+//  HW8_Q1
+//
+//  Created by Yiwen Zhuang on 4/1/21.
+//
+
+#include "Solver03.hpp"
---
+++ HW8_yz6735/HW8_Q2/HW8_Q2/Solver03.cpp
@@ -0,0 +1,8 @@
+//
+//  Solver03.cpp
+//  HW8_Q2
+//
+//  Created by Yiwen Zhuang on 4/1/21.
+//
+
+#include "Solver03.hpp"
